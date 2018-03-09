433ff229820843892a02cfff093bfc2a6a53aa48
Oscar Lugo <oscarlugo@Andrews-iPhone.csusm.edu>
2018-03-08 18:39:12 -0800

    Added DayCalendar List

---
+++ Calendar/daycalendar.h
@@ -0,0 +1,17 @@
+#ifndef DAYCALENDAR_H
+#define DAYCALENDAR_H
+
+#include <list>
+#include <event.h>
+
+class DayCalendar
+{
+private:
+    list<Event> userEvents;
+
+public:
+    DayCalendar();
+
+};
+
+#endif // DAYCALENDAR_H
